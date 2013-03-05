a10c273f101d0ee56e9143936e4e712f45dfe7e8
Pausa90 <andreaiuliano90@gmail.com>
2013-03-05 22:27:17 +0100

    Risolto il problema dello spazio con indentazione

--- learning-javascript/2013-03-05/exercise03.js
+++ learning-javascript/2013-03-05/exercise03.js
@@ -1,9 +1,10 @@
 for (var i = 1; i < 11; i++) {
+	document.write("<div>");
 	for (var j = 1; j < 11; j++){
 		if (i===j)
 			document.write("1," + "\t");
 		else
 			document.write("0," + "\t");
 	}
-	document.writeln();
+	document.write("</div>");
 }
