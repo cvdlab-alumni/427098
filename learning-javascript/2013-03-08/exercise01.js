795708bb53e8208af60cbc85321099c9ee955498
Pausa90 <andreaiuliano90@gmail.com>
2013-03-08 13:04:18 +0100

    esercizio01 completo

--- learning-javascript/2013-03-08/exercise01.js
+++ learning-javascript/2013-03-08/exercise01.js
@@ -1,6 +1,9 @@
 function f(numbers, n){
 	for (var i = 1; i <= n; i++) {
 		numbers.push(i);
-	};
-	console.log(numbers);
+	};	
+	return numbers.filter(function(item, index, array){return item%2===1;})
+					.map(function(item, index, array){return item*2;})
+					.filter(function(item, index, array){return item%4===0})
+					.reduce(function(a,b){return a+b});
 }
