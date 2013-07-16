89e71ebacee0c868df79509966936b74ee6a3bd3
Pausa90 <andreaiuliano90@gmail.com>
2013-07-16 23:34:12 +0200

    avanzamento tazza

--- final-project/tac_tea_pot_cup.js
+++ final-project/tac_tea_pot_cup.js
@@ -58,7 +58,11 @@ function traslaPointsX(points,value){
 	});
 }
 
-
+function rotateProfileNoTraslate(points){
+	var area_domain = PROD1x1([INTERVALS(1)(128),INTERVALS(1)(128)]);
+	var curve_map = BEZIER(S0)(points);
+	return MAP(PROFILEPROD_SURFACE([curve_map,bezier_circle_map(1,S1)]))(area_domain);
+}
 
 
 var line_domain = INTERVALS(1)(32)
@@ -68,11 +72,20 @@ var plate_color_p1 = rgb([60,60,60]);
 var plate_color_p2 = rgb([58,58,58]);
 var cup_color = rgb([300,300,300]);
 
-//incioccettire la tazza
-var cup_points1 = addYValue([[3.86, 1.24], [4.33, 1.32], [5.22, 2.14], [5.11, 3.09]],0);
-cup_points = traslaPointsX(cup_points,-cup_base_traslate);
+var cup_points1 = addYValue([[3.65, 1.16], [4.34, 1.42], [5.07, 1.77], [5.17, 3.11]],0);
+var cup_points2 = addYValue([[3.71, 1.16], [4.28, 1.42], [5.01, 1.77], [5.11, 3.11]],0);
+var cup_points3 = addYValue([[5.17, 3.11],[5.15, 3.15],[5.13, 3.15],[5.11, 3.11]],0);
 
-var cup_external = STRUCT([rotateProfileNoTraslate(cup_points),TNC([2])([minValueCoordinate(cup_points,2)])(circle(3.86-cup_base_traslate+0.05))]); 
+var cup_base_traslate = 3.21;
+
+cup_points1 = traslaPointsX(cup_points1,-cup_base_traslate);
+cup_points2 = traslaPointsX(cup_points2,-cup_base_traslate);
+cup_points3 = traslaPointsX(cup_points3,-cup_base_traslate);
+
+var cup_external = STRUCT([rotateProfileNoTraslate(cup_points1),
+							rotateProfileNoTraslate(cup_points2),
+							rotateProfileNoTraslate(cup_points3),
+							TNC([2])([minValueCoordinate(cup_points1,2)])(circle(3.86-cup_base_traslate+0.05))]); 
 
 
 
@@ -89,10 +102,9 @@ plate_p2 = COLOR(plate_color_p2)(plate_p2);
 
 var plate = STRUCT([plate_p1,plate_p2]);
 
-//plate = SNC([0,1,2])([1.25,1.25,1.25])(plate);
+plate = SNC([0,1,2])([1.25,1.25,1.25])(plate);
 
 
 
-drawBezier(cup_points)
-//draw(cup_external)
-//draw(plate)
+draw(cup_external)
+draw(plate)
